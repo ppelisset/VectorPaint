05836caa17fe27583848e5e9fd012a3dc9c948ba
maxence <maxence.augier@laposte.net>
2013-12-10 16:21:25 +0100

    Ajout ColorButton

---
+++ fr/iutinfo/view/manager/ColorButton.java
@@ -0,0 +1,46 @@
+package fr.iutinfo.view.manager;
+
+import java.awt.Color;
+
+import javax.swing.JButton;
+
+public class ColorButton extends JButton {
+
+	private Color color;
+	private String nomCouleur;
+	public ColorButton(Color c, String nomnomCouleur){
+		this.color=c;
+		this.nomCouleur=nomCouleur;
+		
+		
+	}
+	/**
+	 * retourne le nom de la couleur
+	 * @return String 
+	 */
+	public String getnomCouleur() {
+		return nomCouleur;
+	}
+	/**
+	 * set le nom de la couleur
+	 * @param nomCouleur
+	 */
+	public void setnomCouleur(String nomCouleur) {
+		this.nomCouleur = nomCouleur;
+	}
+	/**
+	 * retourne la couleur
+	 * @return Color
+	 */
+	public Color getColor(){
+		return this.color;
+	}
+	/**
+	 * set la couleur
+	 * @param color
+	 */
+	public void setColor(Color color){
+		
+		this.color=color;
+	}
+}
